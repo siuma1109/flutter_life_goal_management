52fcd1edff04398aac6f7757823d36216d9a2fc6
siuma1109 <matsunkit1109@gmail.com>
2025-02-11 22:36:07 +0800

    add switch navgation history stack for back swipe

--- lib/src/features/bottom_nav/views.widgets/bottom_nav.dart
+++ lib/src/features/bottom_nav/views.widgets/bottom_nav.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
-class BottomNavWidget extends StatelessWidget {
+class BottomNavWidget extends StatefulWidget {
   const BottomNavWidget({
     super.key,
     required this.navigationShell,
@@ -9,22 +9,56 @@ class BottomNavWidget extends StatelessWidget {
 
   final StatefulNavigationShell navigationShell;
 
+  @override
+  State<BottomNavWidget> createState() => _BottomNavWidgetState();
+}
+
+class _BottomNavWidgetState extends State<BottomNavWidget> {
+  final List<int> _navigationStack = [];
+
   void _onTap(BuildContext context, int index) {
-    navigationShell.goBranch(
+    if (index != widget.navigationShell.currentIndex) {
+      setState(() {
+        _navigationStack.add(widget.navigationShell.currentIndex);
+      });
+    }
+
+    widget.navigationShell.goBranch(
       index,
-      initialLocation: index == navigationShell.currentIndex,
+      initialLocation: index == widget.navigationShell.currentIndex,
     );
+  }
+
+  Future<bool> _onWillPop() async {
+    if (_navigationStack.isEmpty) {
+      return true; // Allow app to close
+    }
+
+    setState(() {
+      final lastIndex = _navigationStack.removeLast();
+      widget.navigationShell.goBranch(
+        lastIndex,
+        initialLocation: lastIndex == widget.navigationShell.currentIndex,
+      );
+    });
+    return false; // Prevent app from closing
   }
 
   @override
   Widget build(BuildContext context) {
     return PopScope(
+      canPop: _navigationStack.isEmpty,
+      onPopInvokedWithResult: (bool didPop, Object? result) async {
+        if (!didPop) {
+          await _onWillPop();
+        }
+      },
       child: Scaffold(
-        body: navigationShell,
+        body: widget.navigationShell,
         bottomNavigationBar: BottomNavigationBar(
           onTap: (index) => _onTap(context, index),
           backgroundColor: const Color(0xffe0b9f6),
-          currentIndex: navigationShell.currentIndex,
+          currentIndex: widget.navigationShell.currentIndex,
           items: const [
             BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
             BottomNavigationBarItem(icon: Icon(Icons.login), label: 'Login'),
