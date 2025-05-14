a9f5e1daa1a67dc5613ccbd9e6ab025084c46f9c
siuma1109 <matsunkit1109@gmail.com>
2025-05-15 01:39:25 +0800

    Fix profile project list overflow issue

--- lib/src/widgets/explore/projects_shimmer_loading_widget.dart
+++ lib/src/widgets/explore/projects_shimmer_loading_widget.dart
@@ -9,39 +9,41 @@ class ProjectsShimmerLoadingWidget extends StatelessWidget {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        ListView.builder(
-          shrinkWrap: true,
-          physics: const NeverScrollableScrollPhysics(),
-          itemCount: 6,
-          itemBuilder: (context, index) {
-            return Padding(
-              padding:
-                  const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Row(
-                    children: const [
-                      ShimmerLoadingWidget(
-                        height: 30,
-                        width: 30,
-                        margin: EdgeInsets.only(right: 10),
-                      ),
-                      ShimmerLoadingWidget(
-                        height: 20,
-                        width: 150,
-                      ),
-                    ],
-                  ),
-                  const ShimmerLoadingWidget(
-                    height: 30,
-                    width: 30,
-                    margin: EdgeInsets.zero,
-                  ),
-                ],
-              ),
-            );
-          },
+        Expanded(
+          child: ListView.builder(
+            shrinkWrap: true,
+            physics: const NeverScrollableScrollPhysics(),
+            itemCount: 6,
+            itemBuilder: (context, index) {
+              return Padding(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      children: const [
+                        ShimmerLoadingWidget(
+                          height: 30,
+                          width: 30,
+                          margin: EdgeInsets.only(right: 10),
+                        ),
+                        ShimmerLoadingWidget(
+                          height: 20,
+                          width: 150,
+                        ),
+                      ],
+                    ),
+                    const ShimmerLoadingWidget(
+                      height: 30,
+                      width: 30,
+                      margin: EdgeInsets.zero,
+                    ),
+                  ],
+                ),
+              );
+            },
+          ),
         ),
       ],
     );
